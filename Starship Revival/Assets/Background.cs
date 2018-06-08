5afe8e4e7d5f3fa177035f80077cb2561878e7bf
DrEthan <nckdraper@gmail.com>
2018-06-08 14:49:22 -0600

    Initial Project Commit
    added all the unity project files

---
+++ Starship Revival/Assets/Background.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Background : MonoBehaviour
+{
+
+    public GameObject starprefab;
+    public GameObject nebulaprefab;
+    public GameObject starparent;
+    public GameObject nebulaparent;
+    public int numofnebulas;
+    public int numofneededstars;
+    public List<Color> starcolors;
+    public List<GameObject> starlist;
+    public List<GameObject> nebulalist;
+    public int startingstars;
+    public int scale;
+    public int octaves;
+    public float persistance;
+    public float lucanarity;
+    public Vector2 offset;
+    // Use this for initialization
+    void Start()
+    {
+        nebulas();
+        StartCoroutine(spawnstars());
+    }
+    public IEnumerator spawnstars()
+    {
+        for (int i = 0; i < startingstars; i++)
+        {
+            var temp = Instantiate(starprefab);
+            temp.transform.SetParent(starparent.transform);
+            temp.GetComponent<Image>().color = starcolors[Random.Range(0, starcolors.Count - 1)];
+            Vector2 xrange = new Vector2(GetComponent<Canvas>().pixelRect.xMin, GetComponent<Canvas>().pixelRect.xMax);
+            Vector2 yrange = new Vector2(GetComponent<Canvas>().pixelRect.yMin, GetComponent<Canvas>().pixelRect.yMax);
+
+            temp.transform.position = new Vector3(Random.Range(xrange.x, xrange.y), Random.Range(yrange.x, yrange.y));
+            starlist.Add(temp);
+        }
+        while (true)
+        {
+            if (starlist.Count < numofneededstars)
+            {
+                yield return new WaitForSeconds(Random.Range(0, .5f));
+                var temp = Instantiate(starprefab);
+                temp.transform.SetParent(starparent.transform);
+                temp.GetComponent<Image>().color = starcolors[Random.Range(0, starcolors.Count - 1)];
+                Vector2 xrange = new Vector2(GetComponent<Canvas>().pixelRect.xMin, GetComponent<Canvas>().pixelRect.xMax);
+                temp.transform.position = new Vector3(Random.Range(xrange.x, xrange.y), GetComponent<Canvas>().pixelRect.yMax + 5);
+                starlist.Add(temp);
+            }
+            yield return new WaitUntil(() => starlist.Count < numofneededstars);
+        }
+    }
+
+    public IEnumerator nebulas()
+    {
+        //GameObject nebulaprefab = Instantiate(nebulaprefab);
+        while (true)
+        {
+            if (nebulalist.Count < numofnebulas)
+            {
+                GameObject temp2 = Instantiate(nebulaprefab);
+
+                temp2.transform.SetParent(gameObject.transform);
+                Sprite temp = Sprite.Create(new Texture2D((int)temp2.GetComponent<Image>().GetPixelAdjustedRect().width, (int)temp2.GetComponent<Image>().GetPixelAdjustedRect().height), new Rect(0, 0, (int)temp2.GetComponent<Image>().GetPixelAdjustedRect().width, (int)temp2.GetComponent<Image>().GetPixelAdjustedRect().height), new Vector2(.5f, .5f));
+                Color[] pix = new Color[temp.texture.width * temp.texture.height];
+
+                float[,] noiseMap = Noise.GenerateNoiseMap(temp.texture.width, temp.texture.height, Random.seed, scale, octaves, persistance, lucanarity, offset);
+                int width = noiseMap.GetLength(0);
+                int height = noiseMap.GetLength(1);
+
+                Texture2D texture = new Texture2D(width, height);
+
+                Color[] colourMap = new Color[width * height];
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        colourMap[y * width + x] = Color.Lerp(Color.black, Color.cyan, noiseMap[x, y]);
+                    }
+                }
+                nebulalist.Add(temp2);
+                temp.texture.SetPixels(colourMap);
+                temp2.GetComponent<Image>().sprite = temp;
+            }
+            yield return new WaitUntil(() => numofnebulas < nebulalist.Count);
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Starship Revival/Assets/BackgroundStar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundStar : MonoBehaviour
+{
+    public float movespeed;
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(star());
+    }
+    public IEnumerator star()
+    {
+        movespeed = Random.Range(1f, 2f);
+        //while (GetComponentInParent<Canvas>().pixelRect.yMin - 5 <= transform.position.y)
+        //{
+        GetComponent<Rigidbody2D>().mass = movespeed;
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, -25), ForceMode2D.Impulse);
+        //Debug.Log("alive");
+        //transform.position -= new Vector3(0, movespeed);
+        //yield return new WaitForSeconds(.05f);
+        yield return new WaitUntil(() => GetComponentInParent<Canvas>().pixelRect.yMin - 5 >= transform.position.y);
+        //}
+        //Debug.Log("ded");
+        GetComponentInParent<Background>().starlist.Remove(gameObject);
+        Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
